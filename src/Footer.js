2747f909c04229827110081ecbd4d4b425b1cf16
sushantgaikwad2004 <sushantgaikwad287@gmail.com>
2025-02-13 18:26:04 -0500

    added original links

--- src/Footer.js
+++ src/Footer.js
@@ -8,26 +8,21 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="social-icons">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
-          </a>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} size="2x" />
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/sushant__1106?igsh=ODl6NndyaXFkam9x" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faInstagram} size="2x" />
           </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/sushantgaikwad2004" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} size="2x" /> {/* Fixed faGithub */}
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/sushant-gaikwad-4719032b2/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} size="2x" /> {/* Fixed faLinkedin */}
           </a>
-          <a href="https://www.website.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://sushant-portfoliog.netlify.app/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGlobe} size="2x" />
           </a>
         </div>
-        <p>© 2025 Your Company. All Rights Reserved.</p>
+        <p>Created By Sushant</p>
+        <p>sushantgaikwad287@gmail.com</p>
       </div>
     </footer>
   );
